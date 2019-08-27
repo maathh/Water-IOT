13dd36d0d60ae450f53545767ca268ff22ffc74f
Matheus Alves <matheus.alves.braz.santos@gmail.com>
2019-08-27 00:47:37 -0300

    Update sensorDS18B20.py

--- sensorDS18B20.py
+++ sensorDS18B20.py
@@ -1,18 +1,20 @@
 import os
+import glob
 import time
-
+ 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices'
-device_file = base_dir + '28-021491775f69' + '/w1_slave'
-
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
 def read_temp_raw():
-    f = open(device_file, 'f')
-    lines = f.readLines()
+    f = open(device_file, 'r')
+    lines = f.readlines()
     f.close()
     return lines
-
+ 
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -21,11 +23,10 @@ def read_temp():
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
-        tem_c = float(temp_string) /1000
-        return temp_c
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
     
 while True:
-    print read_temp()
+    print(read_temp())  
     time.sleep(1)
-
-        
